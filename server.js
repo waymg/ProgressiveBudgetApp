db9b32f234f352341549f7ef23852bc50659f4f7
waymg <wayilgmustafa@gmail.com>
2020-10-28 23:30:25 -0400

    .

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://username:password1@ds219308.mlab.com:19308/heroku_9738h7dp", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+// routes here
+app.use(require("./routes/api.js"));
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
